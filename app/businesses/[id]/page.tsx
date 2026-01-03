0c1cd1486cdc8d5900a4bb45074606f2e8eed519
baxtiyorWeb <baxtiyorqurbonnazarov33@gmail.com>
2026-01-04 01:04:07 +0500

    refactor layout styles for improved responsiveness

--- app/businesses/[id]/page.tsx
+++ app/businesses/[id]/page.tsx
@@ -217,7 +217,7 @@ export default function BusinessDetailPage() {
     );
 
   return (
-    <div className="max-w-7xl mx-auto p-4 space-y-6 pb-28 relative min-h-screen">
+    <div className="max-w-dvw mx-auto p-0 lg:p-4 space-y-4 relative min-h-screen">
       {/* Header */}
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-3 min-w-0">
